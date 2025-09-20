c88bc90fc303c54d5db1ee5473ce6c622290b401
Ivo van Poorten <ivopvp@gmail.com>
2025-09-20 18:59:21 +0200

    Add fixup.sh, script to automate some of the conversion

---
+++ tools/fixup.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+if [ "$#" != "1" ]; then
+    echo "usage: $0 file" > /dev/stderr
+    return 1
+fi
+
+sed -i 's/:\\/;/g' "$1"
+sed -i 's/:/\n/g' "$1"
+sed -i 's/&/$/g' "$1"
+sed -i 's/^/    /g' "$1"
+sed -i 's/^    \.L\(.*\)/\1:/g' "$1"
+sed -i 's/ASL A/ASL/g' "$1"
+sed -i 's/ROL A/ROL/g' "$1"
+sed -i 's/LSR A/LSR/g' "$1"
+sed -i 's/ROR A/ROR/g' "$1"
